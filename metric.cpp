56ba810ad92ef27b1f90aee1da39cf413a166b01
Yuzhakov Gleb <socialgraph@yandex.ru>
2020-03-09 20:12:05 +0300

    add metric

---
+++ metric.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Gleb Yuzhakov on 3/9/20.
+//
+
---
+++ sized_roc_auc.h
@@ -0,0 +1,8 @@
+//
+// Created by Gleb Yuzhakov on 3/9/20.
+//
+
+#ifndef SROCAUC_SIZED_ROC_AUC_H
+#define SROCAUC_SIZED_ROC_AUC_H
+
+#endif //SROCAUC_SIZED_ROC_AUC_H
